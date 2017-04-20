aa6dca8ff8512c6a931ad635ae151a7de5b84b29
Chingis Sandanov <chingis@wodby.com>
2017-04-20 12:22:55 +0700

    Always use CWD

--- 4/7.0/init/20-wordpress.sh
+++ 4/7.0/init/20-wordpress.sh
@@ -6,20 +6,13 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
-if [[ "${DOCROOT_SUBDIR}" == "" ]]; then
-	WP_ROOT="${APP_ROOT}"
-else
-	WP_ROOT="${APP_ROOT}/${DOCROOT_SUBDIR}"
-fi
-
-if ! [[ -e "${WP_ROOT}/index.php" ]]; then
-    echo >&2 "WordPress not found in $PWD - copying now..."
-    chown -R www-data:www-data "${WP_ROOT}"
-    rsync -roglt "/usr/src/wordpress/" "${WP_ROOT}/"
+if ! [ -e index.php -a -e wp-includes/version.php ]; then
+    echo >&2 "WordPress not found in ${PWD} - copying now..."
+    chown -R www-data:www-data "${PWD}"
+    rsync -roglt "/usr/src/wordpress/" "${PWD}/"
     echo >&2 "Complete! WordPress has been successfully copied to $PWD"
 
-    if [[ -n "${DB_NAME}" && -n "${DB_USER}" && -n "${DB_PASSWORD}" && -n "${DB_HOST}" ]]; then
-        wp core --path="${WP_ROOT}" config \
-            --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="${DB_HOST}"
+    if [[ -z "${WP_VERSION}" && -n "${DB_NAME}"  ]]; then
+        wp core config --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="${DB_HOST}"
     fi
 fi
