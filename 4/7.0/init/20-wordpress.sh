d437d08f0c537af3fcd22a193b9e765725b374b6
Chingis Sandanov <chingis@wodby.com>
2017-04-20 12:32:45 +0700

    Use APP_ROOT for init

--- 4/7.0/init/20-wordpress.sh
+++ 4/7.0/init/20-wordpress.sh
@@ -6,13 +6,14 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
-if ! [ -e index.php -a -e wp-includes/version.php ]; then
-    echo >&2 "WordPress not found in ${PWD} - copying now..."
-    chown -R www-data:www-data "${PWD}"
-    rsync -roglt "/usr/src/wordpress/" "${PWD}/"
-    echo >&2 "Complete! WordPress has been successfully copied to $PWD"
+if ! [ -e "${APP_ROOT}/index.php" -a -e "${APP_ROOT}/wp-includes/version.php" ]; then
+    echo >&2 "WordPress not found in ${APP_ROOT} - copying now..."
+    chown -R www-data:www-data "${APP_ROOT}"
+    rsync -roglt "/usr/src/wordpress/" "${APP_ROOT}/"
+    echo >&2 "Complete! WordPress has been successfully copied to ${APP_ROOT}"
 
     if [[ -z "${WP_VERSION}" && -n "${DB_NAME}"  ]]; then
-        wp core config --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="${DB_HOST}"
+        wp core --path="${APP_ROOT}" config \
+            --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="${DB_HOST}"
     fi
 fi
