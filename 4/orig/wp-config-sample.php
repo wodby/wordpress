c3ec010e65155c604eddb7446efc30795c0be075
Chingis Sandanov <chingis@wodby.com>
2018-02-22 12:58:18 +0700

    debug travis build

--- 4/orig/wp-config-sample.php
+++ 4/orig/wp-config-sample.php
@@ -1,8 +1,9 @@
 <?php
 /**
  * The base configuration for WordPress
-   *
+ *
  * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
  * copy this file to "wp-config.php" and fill in the values.
  *
  * This file contains the following configurations:
